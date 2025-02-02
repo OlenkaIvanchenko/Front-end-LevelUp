e07ee8762d9836464ca18c1c1585973a55076997
OlenkaIvanchenko <timoshuk.elena97@gmail.com>
2025-02-02 21:27:07 +0200

    add ifElse.html and ifElse.js for conditional logic exercises; implement roulette game logic in roulette.html

---
+++ self_studying/JS_from0_to_profi/ifElse.js
@@ -0,0 +1,11 @@
+let myVariable = false;
+console.log(myVariable);
+if(myVariable){
+    console.log("Hello world!");
+}if(!myVariable){
+    console.log("Goodbye world!");
+}
+
+// Практичне 4.2
+let age = Number(prompt('Уведіть ваш вік: '));
+
