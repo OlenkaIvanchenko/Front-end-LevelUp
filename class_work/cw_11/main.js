4e4f39660175c781cc94df80eddea26c981ed6cf
OlenkaIvanchenko <timoshuk.elena97@gmail.com>
2025-02-16 20:52:05 +0200

    add roulette game implementation with HTML, CSS, and JavaScript; create game state management and user interaction features

---
+++ class_work/cw_11/main.js
@@ -0,0 +1,20 @@
+const gameState = {
+    playerScore: 0,
+    pcScore: 0,
+    timeLimit : 1000,
+}
+
+const elements = {
+    startBatton: null,
+    timeInput: null,
+    playerScoreElement: null,
+    pcScoreElement: null,
+    gameGrid: null,
+    modalOvarlay: null,
+    modalMessage: null,
+    modalClose: null,
+}
+
+function initializeGame() {
+    
+}
